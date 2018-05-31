8a5b929b51cbaf6441d4bfc6e019dc6412e5837a
tiny-tinker <flashingtinker@gmail.com>
2018-05-30 23:24:01 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
+import App from './scenes/app';
+import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
+
+const target = document.querySelector('#root');
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  target
+);
+registerServiceWorker();
---
+++ src/scenes/Home/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = props => <React.Fragment>Welcome Home</React.Fragment>;
+
+export default Home;
